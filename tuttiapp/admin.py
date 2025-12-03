483ac48d71da49ddc82f0f0e59d2d53eb54adcd6
Benson Mose <mosebemos26@gmail.com>
2025-12-03 13:15:20 +0300

    regestered models in admin for easier navigation and modification, we implemented our own models!

--- tuttiapp/admin.py
+++ tuttiapp/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+from django.contrib import admin # Import the admin module used to register models in the admin interface
+from .models import User, Lesson, MpesaTransaction # Import the models we defined in models.py
 
-# Register your models here.
+# This makes the User model visible
+admin.site.register(User) # Register the custom User model so it appears in the admin interface
+
+# This makes the Lesson model visible and adds some filters
+@admin.register(Lesson) # Register the Lesson model with custom admin options
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'teacher', 'student', 'start_time', 'status')
+    list_filter = ('status', 'start_time')
+    search_fields = ('topic', 'student__username')
+
+# This makes the M-Pesa transactions visible
+@admin.register(MpesaTransaction)
+class MpesaTransactionAdmin(admin.ModelAdmin):
+    list_display = ('lesson', 'amount', 'is_successful', 'mpesa_receipt_number')
