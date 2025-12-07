26b60f5b4b155e96ebbdcedad451b5fd6cd2d1b8
Benson Mose <mosebemos26@gmail.com>
2025-12-08 00:01:49 +0300

    superuser logic in duild.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
@@ -8,11 +9,12 @@ pip install -r requirements.txt
 # 2. Collect static files
 python manage.py collectstatic --no-input
 
-# 3. Migrate Database (Fixes missing tables)
+# 3. Migrate Database
 python manage.py migrate
 
-# 4. Auto-Create Superuser (The Shell Bypass)
-python create_superuser.py
+# 4. Create Superuser (Directly here!)
+# This checks if 'admin' exists. If not, it creates admin/admin123
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123', is_teacher=True) if not User.objects.filter(username='admin').exists() else print('Admin already exists')" | python manage.py shell
 
 #i have added whitenoise to settings.py for static files handling on render.com
 #i have also added signup url pattern to tutti/urls.py and created signup.html template for user registration
